4f87a2b85950d89ccca34399577cdfe9b9d7a263
Desmond Chin <chin.sy.des@gmail.com>
2022-04-27 22:28:33 +0800

    complete challenge 5

--- Prototype 5/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Prototype 5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -8,26 +8,29 @@ using UnityEngine.UI;
 public class GameManagerX : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timeText;
     public TextMeshProUGUI gameOverText;
     public GameObject titleScreen;
-    public Button restartButton; 
+    public Button restartButton;
 
     public List<GameObject> targetPrefabs;
 
     private int score;
     private float spawnRate = 1.5f;
+    private int countdownSeconds = 60;
     public bool isGameActive;
 
-    private float spaceBetweenSquares = 2.5f; 
+    private float spaceBetweenSquares = 2.5f;
     private float minValueX = -3.75f; //  x value of the center of the left-most square
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
-    
+
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
+        spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
+        StartCoroutine(CountdownTimer());
         score = 0;
         UpdateScore(0);
         titleScreen.SetActive(false);
@@ -45,7 +48,22 @@ public class GameManagerX : MonoBehaviour
             {
                 Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
             }
-            
+
+        }
+    }
+
+    IEnumerator CountdownTimer()
+    {
+        timeText.text = "Time: " + countdownSeconds;
+        while (isGameActive)
+        {
+            yield return new WaitForSecondsRealtime(1);
+            countdownSeconds -= 1;
+            timeText.text = "Time: " + countdownSeconds;
+            if (countdownSeconds < 1)
+            {
+                GameOver();
+            }
         }
     }
 
@@ -70,14 +88,14 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "Score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
 
